070f597c1e340e77451f14993d2ca2f3ab0426d5
marcin <marcin@marcin-Dell-System-XPS-L502X>
2013-05-13 22:19:01 +0200

    Added simple bag

--- scrabble/src/main/java/pl/edu/uj/tcs/matematycy2013/Bag.java
+++ scrabble/src/main/java/pl/edu/uj/tcs/matematycy2013/Bag.java
@@ -1,29 +1,54 @@
 package pl.edu.uj.tcs.matematycy2013;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class Bag {
 
+    private LinkedList<Letter> bag;
+
     public Bag() {
-        //TODO
+        bag = new LinkedList<Letter>();
     }
 
-    public Bag(InputStream languageFile) {
-        //TODO
+    public Bag(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] t = line.split(" ");
+            bag.add(new Letter(line.charAt(0), Integer.parseInt(t[1])));
+        }
+
+        reader.close();
     }
 
     public int getSize() {
-        //TODO
-        return 0;
+        return bag.size();
     }
 
     public Letter[] getLetters(int amount) {
-        //TODO
-        return null;
+        if (amount > getSize()) {
+            return null;
+        }
+
+        Letter[] letters = new Letter[amount];
+        Collections.shuffle(bag);
+
+        for (int i = 0; i < amount; ++i) {
+            letters[i] = bag.pollLast();
+        }
+
+        return letters;
     }
 
     public void addLetters(Letter[] toAdd) {
-        //TODO
+        for (int i = 0; i < toAdd.length; ++i) {
+            bag.add(toAdd[i]);
+        }
     }
-
 }
